3741af9d32753d24d447537bf0c5945bb36b489a
Vimal1992 <vimal1992@outlook.com>
2018-02-23 08:38:39 +0530

    The base project

---
+++ Threads/src/main/java/test/threads/Threads/App.java
@@ -0,0 +1,13 @@
+package test.threads.Threads;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
