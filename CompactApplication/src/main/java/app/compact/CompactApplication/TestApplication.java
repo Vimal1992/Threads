b1b5ac4a0d169d9c3bd9ea10e2b3d5ed6da7df24
Vimal1992 <vimal1992@outlook.com>
2018-02-27 08:39:05 +0530

    New Compact Application Created

---
+++ CompactApplication/src/main/java/app/compact/CompactApplication/TestApplication.java
@@ -0,0 +1,7 @@
+package app.compact.CompactApplication;
+
+public class TestApplication {
+	public static void main(String[] args) {
+		System.out.println("This is main to test part of the application");
+	}
+}
