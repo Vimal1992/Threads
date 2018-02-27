de6215144ca36129088de6a3b6a9ec7b7f8f5dd2
Vimal1992 <vimal1992@outlook.com>
2018-02-27 08:42:32 +0530

    Update TestApplication.java
    Changed the description inside main() method

--- CompactApplication/src/main/java/app/compact/CompactApplication/TestApplication.java
+++ CompactApplication/src/main/java/app/compact/CompactApplication/TestApplication.java
@@ -2,6 +2,6 @@ package app.compact.CompactApplication;
 
 public class TestApplication {
 	public static void main(String[] args) {
-		System.out.println("This is main to test part of the application");
+		System.out.println("This is a test Program!");
 	}
 }
