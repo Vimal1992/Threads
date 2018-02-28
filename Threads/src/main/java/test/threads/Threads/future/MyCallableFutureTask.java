c95d76b1a33319f27f483e6436611c0887e43f4b
Vimal1992 <vimal1992@outlook.com>
2018-02-28 08:44:43 +0530

    Added Future Callable examples

---
+++ Threads/src/main/java/test/threads/Threads/future/MyCallableFutureTask.java
@@ -0,0 +1,20 @@
+package test.threads.Threads.future;
+
+import java.util.concurrent.Callable;
+
+public class MyCallableFutureTask implements Callable<String> {
+
+	private long waitTime;
+
+	public MyCallableFutureTask(int timeInMillis) {
+		this.waitTime = timeInMillis;
+	}
+
+	@Override
+	public String call() throws Exception {
+		Thread.sleep(waitTime);
+		// return the thread name executing this callable task
+		return Thread.currentThread().getName();
+	}
+
+}
